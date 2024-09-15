e5b580ea5eb8976d55df1fdaf8b5aa2d50a61ecd
Shakil Ahmed <shakil200409@gmail.com>
2024-09-16 00:59:21 +0600

    tried to display odd numbers without the numbers which are divisible by 5

---
+++ five-skip.js
@@ -0,0 +1,16 @@
+for(let i=55; i<=85;i+=2){
+    if(i%5===0){
+        continue;
+    }
+    console.log(i);
+}
+
+
+// let i = 55;
+// while(i<=85){
+//     if(i%5===0){
+//         continue;
+//     }
+//     console.log(i);
+//     i+=2;
+// }
